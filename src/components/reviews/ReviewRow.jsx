904433f224d382a374d289622d373519b327fbf4
Simon Criado <50947741+simoncriado@users.noreply.github.com>
2022-12-23 10:38:55 +0100

    Finish contact page. Created table to display reviews

---
+++ src/components/reviews/ReviewRow.jsx
@@ -0,0 +1,54 @@
+// React & Router
+import React from "react";
+
+// Styles
+import {
+  Row,
+  DataContainer,
+  DataContainerButton,
+  GuestContainer,
+  GuestName,
+  BookingID,
+  Status,
+  NotesButton,
+} from "./ReviewRowStyled";
+
+// Component that creates a table row for the bookings table
+export const ReviewRow = ({ review }) => {
+  const stars = [];
+  let i = 0;
+  while (i < review.stars) {
+    stars.push(
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="30"
+        width="30"
+        viewBox="0 0 48 48"
+      >
+        <path d="m11.65 44 3.25-14.05L4 20.5l14.4-1.25L24 6l5.6 13.25L44 20.5l-10.9 9.45L36.35 44 24 36.55Z" />
+      </svg>
+    );
+    i++;
+  }
+  return (
+    <Row>
+      <DataContainer className="data-container__text">
+        <p>#{review.id}</p>
+      </DataContainer>
+      <DataContainer className="data-container__text">
+        <p>{review.date}</p>
+      </DataContainer>
+      <DataContainer className="data-container__text">
+        <p>{review.user.name}</p>
+      </DataContainer>
+      <DataContainer className="data-container__text">
+        <div>{stars}</div>
+        <p>{review.message.body}</p>
+      </DataContainer>
+      <DataContainerButton>
+        <button className="green">Publish</button>
+        <button className="red">Archive</button>
+      </DataContainerButton>
+    </Row>
+  );
+};
--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,19 +1,134 @@
-import React from "react";
+// React
+import React, { useState, useEffect } from "react";
+
+// Local data
+import ReviewsList from "../../data/reviews";
 
 // Styles
 import { Reviews } from "../dashboard/DashboardStyled";
 import { Container } from "../../components/styled/Containers";
+import {
+  Table,
+  HeaderTitle,
+  TableActions,
+  TableFilters,
+  FilterButton,
+  TableButtons,
+} from "../../components/styled/Tables";
+import { CreateButton } from "../../components/styled/Buttons";
+import { DropdownMenu } from "../../components/styled/DropdownMenu";
 
 // Components
 import ReviewsSwiper from "../../components/reviews/Reviews";
+import { ReviewRow } from "../../components/reviews/ReviewRow";
+import { Pagination } from "../../components/pagination/Pagination";
 
 const Contact = () => {
+  const [reviews, setReviews] = useState(ReviewsList);
+  const [activeFilter, setActiveFilter] = useState("");
+  const [currentReviews, setCurrentReviews] = useState([]);
+
+  const getAllReviews = () => {
+    setReviews(ReviewsList);
+  };
+
+  const filterByType = (type) => {
+    setReviews(ReviewsList.filter((review) => review.archived === type));
+  };
+
+  // useEffect(() => {
+  //   // Filtering by dropdown selection based on the filtered by search input array (in case the user used the search bar)
+  //   const orderedReviews = [...ReviewsList];
+  //   switch (activeFilter) {
+  //     case "Room Nr.":
+  //       orderedReviews.sort((a, b) => a.room_number - b.room_number);
+  //       break;
+  //     case "Highest rate first":
+  //       orderedReviews.sort((a, b) => b.room_rate - a.room_rate);
+  //       break;
+  //     case "Lowest rate first":
+  //       orderedReviews.sort((a, b) => a.room_rate - b.room_rate);
+  //       break;
+  //     default:
+  //       break;
+  //   }
+  //   setReviews(orderedReviews);
+  // }, [activeFilter]);
+
+  // Variables for the pagination component
+  const [currentPage, setCurrentPage] = useState(1);
+  const [roomsPerPage] = useState(10);
+  const indexOfLastImage = currentPage * roomsPerPage; // For example: let´s say we have 17 pages. indexOfLastImage = 17 * roomsPerPage
+  const indexOfFirstImage = indexOfLastImage - roomsPerPage; // Following same example: indexOfFirstImage = indexOfLastPage – roomsPerPage
+  // Setting the current displayed images
+  useEffect(() => {
+    setCurrentReviews(reviews.slice(indexOfFirstImage, indexOfLastImage));
+  }, [reviews, indexOfFirstImage, indexOfLastImage]);
+
+  // Images to be displayed on the current page. slice(96, 102) will return images from index 96 to 101
+  const nPages = Math.ceil(reviews.length / roomsPerPage);
+
   return (
-    <Container>
-      <Reviews>
-        <ReviewsSwiper></ReviewsSwiper>
-      </Reviews>
-    </Container>
+    <>
+      <Container>
+        <Reviews>
+          <ReviewsSwiper></ReviewsSwiper>
+        </Reviews>
+      </Container>
+      <TableActions>
+        <TableFilters>
+          <FilterButton onClick={getAllReviews}>
+            All Customer Reviews
+          </FilterButton>
+          <FilterButton onClick={() => filterByType(false)}>
+            Published
+          </FilterButton>
+          <FilterButton onClick={() => filterByType(true)}>
+            Archived
+          </FilterButton>
+        </TableFilters>
+        <TableButtons>
+          <DropdownMenu
+            setActiveFilter={setActiveFilter}
+            type="white"
+            options={["Newest"]}
+          ></DropdownMenu>
+        </TableButtons>
+      </TableActions>
+      <Container>
+        <Table>
+          <thead>
+            <tr>
+              <HeaderTitle>Order ID</HeaderTitle>
+              <HeaderTitle>Date</HeaderTitle>
+              <HeaderTitle>Customer</HeaderTitle>
+              <HeaderTitle>Comment</HeaderTitle>
+              <HeaderTitle>Action</HeaderTitle>
+            </tr>
+          </thead>
+          <tbody className="task-container">
+            {currentReviews.length > 0 &&
+              currentReviews.map((review, index) => (
+                <ReviewRow
+                  key={review.id}
+                  index={index}
+                  review={review}
+                  number={review.id}
+                />
+              ))}
+          </tbody>
+        </Table>
+      </Container>
+      <Pagination
+        nPages={nPages}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        dataDisplayed={"reviews"}
+        totalRooms={reviews.length}
+        indexOfFirstImage={indexOfFirstImage}
+        indexOfLastImage={indexOfLastImage}
+      />
+    </>
   );
 };
 
