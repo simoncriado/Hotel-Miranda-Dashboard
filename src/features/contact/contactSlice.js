54f9f93540607923ebd860f6093243ae59fd3669
Simon Criado <50947741+simoncriado@users.noreply.github.com>
2023-01-19 14:50:27 +0100

    Adding reviews slice and functionality to that page

---
+++ src/features/contact/contactSlice.js
@@ -0,0 +1,36 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { fetchData } from "../fetchData";
+
+export const getDataReviews = createAsyncThunk(
+  "reviews/fetchReviews",
+  async () => {
+    return await fetchData("Reviews");
+  }
+);
+
+// Defining the initial state using the types declared above
+const initialState = {
+  reviewsList: [],
+  status: "loading",
+};
+
+export const reviewsSlice = createSlice({
+  name: "reviews",
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(getDataReviews.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(getDataReviews.fulfilled, (state, action) => {
+        state.status = "success";
+        state.reviewsList = action.payload;
+      })
+      .addCase(getDataReviews.rejected, (state) => {
+        state.status = "failed";
+        console.error("Not possible to fetch the bookings");
+      });
+  },
+});
+
+export default reviewsSlice.reducer;
--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,8 +1,9 @@
 // React
 import React, { useState, useEffect } from "react";
 
-// Local data
-import ReviewsList from "../../data/reviews";
+// Redux
+import { useDispatch, useSelector } from "react-redux";
+import { getDataReviews } from "../../features/contact/contactSlice";
 
 // Styled Components
 import { Reviews } from "../dashboard/DashboardStyled";
@@ -21,38 +22,67 @@ import { DropdownMenu } from "../../components/styled/DropdownMenu";
 import ReviewsSwiper from "../../components/reviews/Reviews";
 import { ReviewRow } from "../../components/reviews/ReviewRow";
 import { Pagination } from "../../components/pagination/Pagination";
+import { Loader } from "../../components/styled/Loader";
 
 const Contact = () => {
-  const [reviews, setReviews] = useState(ReviewsList);
-  const [activeFilter, setActiveFilter] = useState("");
+  const dispatch = useDispatch();
+  const { reviewsList } = useSelector((state) => state.contactReducer);
+  const { status } = useSelector((state) => state.contactReducer);
+
+  const [reviews, setReviews] = useState(reviewsList);
+  const [activeFilter, setActiveFilter] = useState("Date");
   const [currentReviews, setCurrentReviews] = useState([]);
 
+  // Faking a delay on data fetch
+  useEffect(() => {
+    if (reviewsList.length === 0) {
+      setTimeout(() => {
+        dispatch(getDataReviews());
+      }, 1000);
+    }
+    setReviews(reviewsList);
+  }, [reviewsList, dispatch]);
+
   const getAllReviews = () => {
-    setReviews(ReviewsList);
+    setReviews(reviewsList);
   };
 
   const filterByType = (type) => {
-    setReviews(ReviewsList.filter((review) => review.archived === type));
+    setReviews(reviewsList.filter((review) => review.archived === type));
   };
 
-  // useEffect(() => {
-  //   // Filtering by dropdown selection based on the filtered by search input array (in case the user used the search bar)
-  //   const orderedReviews = [...ReviewsList];
-  //   switch (activeFilter) {
-  //     case "Room Nr.":
-  //       orderedReviews.sort((a, b) => a.room_number - b.room_number);
-  //       break;
-  //     case "Highest rate first":
-  //       orderedReviews.sort((a, b) => b.room_rate - a.room_rate);
-  //       break;
-  //     case "Lowest rate first":
-  //       orderedReviews.sort((a, b) => a.room_rate - b.room_rate);
-  //       break;
-  //     default:
-  //       break;
-  //   }
-  //   setReviews(orderedReviews);
-  // }, [activeFilter]);
+  useEffect(() => {
+    const orderedReviews = [...reviewsList];
+    switch (activeFilter) {
+      case "Date":
+        orderedReviews.sort((a, b) => {
+          let dateA = a.date;
+          let dateB = b.date;
+          if (dateB.split("-").join() < dateA.split("-").join()) {
+            return -1;
+          } else {
+            return 1;
+          }
+        });
+        break;
+      case "User":
+        orderedReviews.sort((a, b) => {
+          const nameA = a.user.name.toUpperCase().replace(/\s/g, "");
+          const nameB = b.user.name.toUpperCase().replace(/\s/g, "");
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
+          return 0;
+        });
+        break;
+      default:
+        break;
+    }
+    setReviews(orderedReviews);
+  }, [activeFilter, reviewsList]);
 
   // Variables for the pagination component
   const [currentPage, setCurrentPage] = useState(1);
@@ -69,64 +99,70 @@ const Contact = () => {
 
   return (
     <>
-      <Container>
-        <Reviews>
-          <ReviewsSwiper></ReviewsSwiper>
-        </Reviews>
-      </Container>
-      <TableActions>
-        <TableFilters>
-          <FilterButton onClick={getAllReviews}>
-            All Customer Reviews
-          </FilterButton>
-          <FilterButton onClick={() => filterByType(false)}>
-            Published
-          </FilterButton>
-          <FilterButton onClick={() => filterByType(true)}>
-            Archived
-          </FilterButton>
-        </TableFilters>
-        <TableButtons>
-          <DropdownMenu
-            setActiveFilter={setActiveFilter}
-            type="white"
-            options={["Newest"]}
-          ></DropdownMenu>
-        </TableButtons>
-      </TableActions>
-      <Container>
-        <Table>
-          <thead>
-            <tr>
-              <HeaderTitle>Order ID</HeaderTitle>
-              <HeaderTitle>Date</HeaderTitle>
-              <HeaderTitle>Customer</HeaderTitle>
-              <HeaderTitle>Comment</HeaderTitle>
-              <HeaderTitle>Action</HeaderTitle>
-            </tr>
-          </thead>
-          <tbody className="task-container">
-            {currentReviews.length > 0 &&
-              currentReviews.map((review, index) => (
-                <ReviewRow
-                  key={review.id}
-                  index={index}
-                  review={review}
-                  number={review.id}
-                />
-              ))}
-          </tbody>
-        </Table>
-      </Container>
-      <Pagination
-        nPages={nPages}
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-        dataDisplayed={"reviews"}
-        totalRooms={reviews.length}
-        indexOfFirstImage={indexOfFirstImage}
-        indexOfLastImage={indexOfLastImage}
-      />
+      {status === "loading" ? (
+        <Loader />
+      ) : (
+        <>
+          <Container>
+            <Reviews>
+              <ReviewsSwiper></ReviewsSwiper>
+            </Reviews>
+          </Container>
+          <TableActions>
+            <TableFilters>
+              <FilterButton onClick={getAllReviews}>
+                All Customer Reviews
+              </FilterButton>
+              <FilterButton onClick={() => filterByType(false)}>
+                Published
+              </FilterButton>
+              <FilterButton onClick={() => filterByType(true)}>
+                Archived
+              </FilterButton>
+            </TableFilters>
+            <TableButtons>
+              <DropdownMenu
+                setActiveFilter={setActiveFilter}
+                type="white"
+                options={["Date", "User"]}
+              ></DropdownMenu>
+            </TableButtons>
+          </TableActions>
+          <Container>
+            <Table>
+              <thead>
+                <tr>
+                  <HeaderTitle>Order ID</HeaderTitle>
+                  <HeaderTitle>Date</HeaderTitle>
+                  <HeaderTitle>Customer</HeaderTitle>
+                  <HeaderTitle>Comment</HeaderTitle>
+                  <HeaderTitle>Action</HeaderTitle>
+                </tr>
+              </thead>
+              <tbody className="task-container">
+                {currentReviews.length > 0 &&
+                  currentReviews.map((review, index) => (
+                    <ReviewRow
+                      key={review.id}
+                      index={index}
+                      review={review}
+                      number={review.id}
+                    />
+                  ))}
+              </tbody>
+            </Table>
+          </Container>
+          <Pagination
+            nPages={nPages}
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+            dataDisplayed={"reviews"}
+            totalRooms={reviews.length}
+            indexOfFirstImage={indexOfFirstImage}
+            indexOfLastImage={indexOfLastImage}
+          />
+        </>
+      )}
     </>
   );
 };
